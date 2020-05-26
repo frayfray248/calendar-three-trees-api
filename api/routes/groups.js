693ffdbbb7255ef0c6ac1fd3b421cdb7187a2e96
frayfray248 <nicman248@hotmail.com>
2020-05-26 16:57:47 -0700

    Added nodemon and morgan

--- api/routes/groups.js
+++ api/routes/groups.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const events = require('./events');
 
+
+// reroute the request to events endpoint
 router.use('/:groupId/events', (req, res, next) => {
     req.groupId = req.params.groupId;
     next();
--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-// database connection
-const database = require('./database.js');
+// routes
+const groupRoutes = require('./api/routes/groups.js');
 
 // open api documentation
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const OAS = YAML.load('./calendar-three-trees-doc.yml');
 
-// routes
-const groupRoutes = require('./api/routes/groups.js');
-
+// using swagger documentation
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(OAS));
 
+// using morgan logging
+app.use(morgan('dev'));
+
+// routes
 app.use('/groups', groupRoutes);
 
 module.exports = app;
