7b0b956f12fc8e682940f88ebea6bf5790dcc16c
frayfray248 <nicman248@hotmail.com>
2020-05-26 16:49:55 -0700

    Added all endpoint placeholders

--- api/routes/events.js
+++ api/routes/events.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/:eventId', (req, res, next) => {
+// add an event 
+router.post('/', (req, res, next) => {
     const eventId = req.params.eventId;
     const groupId = req.groupId;
 
     res.status(200).json({
-        message: `Handling POST requests to groups/${groupId}/events/${eventId}`
+        message: `Handling POST requests to groups/${groupId}/events/`
+    });
+});
+
+// get all events
+router.get('/', (req, res, next) => {
+    const eventId = req.params.eventId;
+    const groupId = req.groupId;
+
+    res.status(200).json({
+        message: `Handling GET requests to groups/${groupId}/events/`
+    });
+});
+
+// delete an event
+router.delete('/:eventId', (req, res, next) => {
+    const eventId = req.params.eventId;
+    const groupId = req.groupId;
+
+    res.status(200).json({
+        message: `Handling DELETE requests to groups/${groupId}/events/${eventId}`
+    });
+});
+
+
+// put an event (update)
+router.put('/:eventId', (req, res, next) => {
+    const eventId = req.params.eventId;
+    const groupId = req.groupId;
+
+    res.status(200).json({
+        message: `Handling PUT requests to groups/${groupId}/events/${eventId}`
+    });
+});
+
+// get an event
+router.get('/:eventId', (req, res, next) => {
+    const eventId = req.params.eventId;
+    const groupId = req.groupId;
+
+    res.status(200).json({
+        message: `Handling PUT requests to groups/${groupId}/events/${eventId}`
     });
 });
 
