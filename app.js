cc16288054fe7d2719064c01b2b969ec2d2641e8
frayfray248 <nicman248@hotmail.com>
2020-05-26 17:04:37 -0700

    Added error handling for unimplemented routes

--- app.js
+++ app.js
@@ -19,4 +19,21 @@ app.use(morgan('dev'));
 // routes
 app.use('/groups', groupRoutes);
 
+// error message for all requests that get passed the implemented routes
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status) || 500;
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
+
 module.exports = app;
