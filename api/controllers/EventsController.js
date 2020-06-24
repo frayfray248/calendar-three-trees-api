8068c82221f1e001ec3d7205604048003109a8fe
frayfray248 <nicman248@hotmail.com>
2020-06-23 17:13:11 -0700

    added updated event endpoint

--- api/controllers/EventsController.js
+++ api/controllers/EventsController.js
@@ -90,7 +90,7 @@ exports.getEvents = (req, res, next) => {
             const contentTermsWhereOption = ((c) => {
                 if (!c) return {};
                 try {
-                    return { content: {[Op.substring]: JSON.parse(c)} };
+                    return { content: { [Op.substring]: JSON.parse(c) } };
                 } catch (err) {
                     throw new Error('bad query');
                 }
@@ -99,13 +99,15 @@ exports.getEvents = (req, res, next) => {
             // building sequelize query where option to search for events with dates between
             // two dates
             const dateRangeWhereOption = ((dr) => {
-                if(!dr) return {};
+                if (!dr) return {};
                 try {
-                    return { [Op.or]: {
-                        startDate: {[Op.between]: [JSON.parse(dr)[0], JSON.parse(dr)[1]]},
-                        endDate: {[Op.between]: [JSON.parse(dr)[0], JSON.parse(dr)[1]]}
-                    }}
-                } catch(err) {
+                    return {
+                        [Op.or]: {
+                            startDate: { [Op.between]: [JSON.parse(dr)[0], JSON.parse(dr)[1]] },
+                            endDate: { [Op.between]: [JSON.parse(dr)[0], JSON.parse(dr)[1]] }
+                        }
+                    }
+                } catch (err) {
                     throw new Error('bad query')
                 }
             })(req.query.dateRange);
@@ -113,17 +115,19 @@ exports.getEvents = (req, res, next) => {
             // building sequelize query where option to search for events with dates matching 
             // any date in an array of dates
             const datesWhereOption = ((d) => {
-                if(!d) return {};
+                if (!d) return {};
                 try {
-                    return { [Op.or]: {
-                        startDate: JSON.parse(d),
-                        endDate: JSON.parse(d),
-                    }}
-                } catch(err) {
+                    return {
+                        [Op.or]: {
+                            startDate: JSON.parse(d),
+                            endDate: JSON.parse(d),
+                        }
+                    }
+                } catch (err) {
                     throw new Error('bad query')
                 }
             })(req.query.dates);
-           
+
             const events = await Event.findAll({
                 where: {
                     programId: req.groupId,
@@ -247,12 +251,140 @@ exports.deleteEvent = (req, res, next) => {
 exports.updateEvent = (req, res, next) => {
     (async () => {
 
+        const transaction = await db.transaction();
+
+        try {
+
+            //save or retrieve location
+            const newLocation = await (async (locationId) => {
+                if (locationId > 0) {
+                    return await Location.findByPk(locationId);
+                } else {
+                    const newLocation = Location.build(
+                        { ...req.body.location });
+                    return await newLocation.save();
+                }
+            })(req.body.location.locationId);
+
+            // getting event to be updated
+            const event = await Event.findByPk(req.params.eventId,
+                {
+                    where: {programId: req.param.groupId}
+                });
+
+            // throw error if not found
+            if (!event) throw new Error('event not found');
+
+            // updating event
+            const updatedEvent = await event.update({
+                ...req.body.event,
+                locationId: newLocation.id,
+                programId: req.params.groupId,
+            });
+
+            const tags = req.body.tags;
+
+            // updating tags
+            for (var i = 0; i < tags.length; i++) {
+                const tag = await Tag.findOrCreate({ where: { name: tags[i] }});
+                await EventTag.findOrCreate({
+                    where: {
+                        eventId: updatedEvent.id,
+                        tagId: tag[0].id,
+                        transaction: transaction
+                    }
+                })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            }
+
+            await transaction.commit();
+            res.status(204).send();
+
+        } catch (err) {
+
+            await transaction.rollback();
+
+            if (err.message === 'event not found') {
+                res.status(404).send('Event not found');
+            } else {
+                res.status(500).send('Internal server error');
+            }
+
+            console.log(err);
+        }
     })();
 }
 
 // get a single event by event id and group id
 exports.getEvent = (req, res, next) => {
     (async () => {
+
+        const transaction = await db.transaction();
+
+        try {
+
+            const eventId = req.params.eventId;
+
+            const event = await Event.findByPk(eventId,
+                {
+                    where: {
+                        programId: req.groupId,
+                    },
+                    include: [
+                        {
+                            model: Location,
+                            as: 'location'
+                        },
+                        {
+                            model: Tag,
+                            as: 'tags',
+                        }
+                    ]
+                });
+
+            // throw error if not found
+            if (!event) throw new Error('event not found');
+
+            const formatedEvent = {
+                event: {
+                    id: event.id,
+                    name: event.name,
+                    content: event.content,
+                    startDate: event.startDate,
+                    endDate: event.endDate,
+                    moreInfoUrl: event.moreInfoUrl
+                },
+                location: {
+                    locationId: event.location.id,
+                    name: event.location.name,
+                    address: event.location.address,
+                    postalCode: event.location.postalCode
+                },
+                tags: event.tags.map(tag => tag.name)
+            };
+
+            await transaction.commit();
+
+            await res.status(200).send({
+                event: formatedEvent
+            });
+
+        } catch (err) {
+            await transaction.rollback();
+
+            if (err.message === 'event not found') {
+                res.status(404).send('Event not found');
+            } else {
+
+                res.status(500).send('Internal server error');
+            }
+
+            console.log(err);
+        }
+
+
 
     })();
 }
